504866bb0bde906f56d3a3c3ef0bb84ed59e2960
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-02-11 17:11:08 -0500

    setup init state

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import './main.scss';
 
 const Header = () => {
+    const [width, setWidth] = useState();
+
+    useEffect(() => {
+        let handleResize = () => {
+            setWidth(window.innerWidth);
+        }
+        window.addEventListener( 'DOMContentLoaded', handleResize );
+        window.addEventListener( 'resize', handleResize );
+
+    }, []);
+
+    const setVisibility = () => {
+        if( width < 750 ){
+            return "hidden";
+        } else if (width > 750){
+            return "";
+        }
+    }
+
+    setVisibility();
+
     return(
         <header className="header max-width">
             <div>
@@ -13,7 +34,7 @@ const Header = () => {
                     <li>
                         <a href="#">Projects</a>
                     </li>
-                    <li>
+                    <li className={setVisibility()}>
                         <a href="mailto:contact@bryanstephens.ca">contact@bryanstephens.ca</a>
                     </li>
                 </ul>
