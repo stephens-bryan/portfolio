95f00f3ccafdab3ab81289c4df006d99ae2fed10
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-02-11 17:06:32 -0500

    setup jumplink, init state

--- src/components/IntroHero/index.js
+++ src/components/IntroHero/index.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './main.scss';
 
 const IntroHero = () => {
+
+    const [height, setHeight] = useState();
+
+    useEffect(() => {
+        let handleResize = () => {
+            setHeight(window.innerHeight);
+        }
+        window.addEventListener( 'DOMContentLoaded', handleResize );
+        window.addEventListener( 'resize', handleResize );
+
+    }, []);
+
+    const jumpToNextSection = () => (
+        window.scrollTo({
+            top: window.innerHeight + 100,
+            behaviour: "smooth"
+        })
+    );
+
     return(
+        
         <section className="intro max-width">
-        <div className="intro_hero__title">
+        <div className="intro_hero__title" style={{height: height}}>
             <p>I’m a Web Developer living in Toronto, Canada. I build web accessbile applications</p>
             <p>Have a Question?</p>
             <p>Reach me at
-                <a href="mailto:contact@bryanstephens.ca">contact@bryanstphens.ca</a>
+                <a href="mailto:contact@bryanstephens.ca"> contact@bryanstphens.ca</a>
             </p>
             <div>
-                <a href="#!">
-                    <svg className="bounce clickable" width="33" height="30" xmlns="http://www.w3.org/2000/svg">
+                 <svg onClick={jumpToNextSection} className="bounce clickable" width="33" height="30" xmlns="http://www.w3.org/2000/svg">
                         <polygon points="0.645,10.383 16,23.871 31.355,10.383 29.375,8.129 16,19.878 2.625,8.129 ">
                         </polygon>
                     </svg>
                     <small className="hidden">
                         Jump to next section
                     </small>
-                </a>
             </div>
         </div>
     </section>
