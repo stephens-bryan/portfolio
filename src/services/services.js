682d031ee3fc344e94511fa5927a4541eb9415d3
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-02-12 16:12:41 -0500

    Added Services Content (V1)

---
+++ src/services/services.js
@@ -0,0 +1,13 @@
+export default {
+    heading: "About Me",
+    img: {
+        src: "/images/AM2_3704.jpg",
+        alt: "Gift the Code 2020, credit Arthur Mola - https://www.instagram.com/arthurmola/?hl=en"
+    },
+    content: [
+        {   heading: "Front-End Development",
+            text: "I am a web developer with 2+ years in developing, testing, and debugging accessible, database-driven web sites and applications. I love exploring new technologies and developing my skills any chance I get."},
+        {   heading: "Accessibility",
+            text: "When not working on the web, I consider myself to be a bit of a gastronome and love to explore new restuarants and cusines when I get a chance."}
+    ]
+}
