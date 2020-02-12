686db8e09bcf56313cc6c9aa9d6715615f88ca25
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-02-12 16:12:31 -0500

    added about content (V1)

---
+++ src/services/about.js
@@ -0,0 +1,11 @@
+export default {
+    heading: "About Me",
+    img: {
+        src: "/images/IMG_8949.jpg",
+        alt: "me and fiance at Lake Louise, Alberta"
+    },
+    content: [
+        { text: "I am a web developer with 2+ years in developing, testing, and debugging accessible, database-driven web sites and applications. I love exploring new technologies and developing my skills any chance I get."},
+        { text: "When not working on the web, I consider myself to be a bit of a gastronome and love to explore new restuarants and cusines when I get a chance."}
+    ]
+}
