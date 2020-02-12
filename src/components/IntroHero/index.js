f92f9da8f0eafbe838b31e68f4955d7d8679ef6f
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-02-12 16:11:49 -0500

    rmv space

--- src/components/IntroHero/index.js
+++ src/components/IntroHero/index.js
@@ -22,25 +22,24 @@ const IntroHero = () => {
     );
 
     return(
-        
         <section className="intro max-width">
-        <div className="intro_hero__title" style={{height: height}}>
-            <p>I’m a Web Developer living in Toronto, Canada. I build web accessbile applications</p>
-            <p>Have a Question?</p>
-            <p>Reach me at
-                <a href="mailto:contact@bryanstephens.ca"> contact@bryanstphens.ca</a>
-            </p>
-            <div>
-                 <svg onClick={jumpLink} className="bounce clickable" width="33" height="30" xmlns="http://www.w3.org/2000/svg">
-                        <polygon points="0.645,10.383 16,23.871 31.355,10.383 29.375,8.129 16,19.878 2.625,8.129 ">
-                        </polygon>
-                    </svg>
-                    <small className="hidden">
-                        Jump to next section
-                    </small>
+            <div className="intro_hero__title" style={{height: height}}>
+                <p>I’m a Web Developer living in Toronto, Canada. I build web accessbile applications</p>
+                <p>Have a Question?</p>
+                <p>Reach me at
+                    <a href="mailto:contact@bryanstephens.ca"> contact@bryanstphens.ca</a>
+                </p>
+                <div>
+                    <svg onClick={jumpLink} className="bounce clickable" width="33" height="30" xmlns="http://www.w3.org/2000/svg">
+                            <polygon points="0.645,10.383 16,23.871 31.355,10.383 29.375,8.129 16,19.878 2.625,8.129 ">
+                            </polygon>
+                        </svg>
+                        <small className="hidden">
+                            Jump to next section
+                        </small>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
     );
 }
 
