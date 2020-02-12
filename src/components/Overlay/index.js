3fc50ba6c2152fd0d6aa035c4ad1b414f36e6cd7
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-02-12 16:12:11 -0500

    added markup

--- src/components/Overlay/index.js
+++ src/components/Overlay/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const Overlay = () => {
+import './main.scss';
+
+
+/**
+ * PROPS
+ * - heading
+ * - img
+ *   - src
+ *   - alt
+ * - text
+ *   - points
+ */
+
+const Overlay = (props) => {
+    
     return(
-        <section>
-            overlay
+        <section className="max-width text component override-top">
+            <div className="main-parent-container overlay-container">
+                <h3 className="overlay-container-heading">{props.props.heading}</h3>
+                <div className="overlay-inner-container">
+                    <img className="img-overlap" src={props.props.img.src} alt={props.props.img.alt} />
+                    <div className="text-overlay">
+                        {props.props.content.map((content, i) => {
+                            return <p key={i}>{content.text}</p>;
+                        })}
+                    </div>
+                </div>
+            </div>
         </section>
     )
 };
