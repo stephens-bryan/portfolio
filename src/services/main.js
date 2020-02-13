5c0a454b66ce5544569e2b87dcfbc26560c2e107
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-02-13 15:50:21 -0500

    added main service

---
+++ src/services/main.js
@@ -0,0 +1,72 @@
+export default {
+    content: [
+        {
+            heading: 'Languages',
+            preamble: 'I have experience with a variety of programming languages, both front-end and back-end oriented.',
+            hexagonContent: [
+                {
+                    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg',
+                    imgTitle: 'W3C [CC BY (https://creativecommons.org/licenses/by/3.0)]',
+                    imgAlt: 'HTML5-logo',
+                    helperText: 'HTML5'
+                },
+                {
+                    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg',
+                    imgTitle: 'Rudloff [CC BY (https://creativecommons.org/licenses/by/3.0)]',
+                    imgAlt: 'CSS3-logo',
+                    helperText: 'CSS3'
+                },
+                {
+                    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+                    imgTitle: 'Ramaksoud2000 via Chris Williams [Public domain]',
+                    imgAlt: 'JavaScript-logo',
+                    helperText: 'JavaScript'
+                }
+            ]
+        },
+        {
+            hexagonContent: [
+                {
+                    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg',
+                    imgTitle: 'Colin Viebrock [CC BY-SA (https://creativecommons.org/licenses/by-sa/4.0)]',
+                    imgAlt: 'PHP-logo',
+                    helperText: 'PHP'
+                },
+                {
+                    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png',
+                    imgTitle: 'Andres15alvarez [CC BY-SA (https://creativecommons.org/licenses/by-sa/4.0)]',
+                    imgAlt: 'CSharp-logo',
+                    helperText: 'C#'
+                },
+                {
+                    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg',
+                    imgTitle: 'node.js authors [Public domain]',
+                    imgAlt: 'Nodejs-logo',
+                    helperText: 'Node.js'
+                }
+            ]
+        },
+        {
+            hexagonContent: [
+                {
+                    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Antu_mongodb.svg/512px-Antu_mongodb.svg.png',
+                    imgTitle: 'Fabián Alexis [CC BY-SA (https://creativecommons.org/licenses/by-sa/3.0)]',
+                    imgAlt: 'MongoDB-logo',
+                    helperText: 'MongoDB'
+                },
+                {
+                    imgSrc: './images/microsoft-sql-server-seeklogo.com.svg',
+                    imgTitle: 'Seeklogo.com [CC https://seeklogo.com/vector-logo/298266/microsoft-sql-server]',
+                    imgAlt: 'MSSQL-logo',
+                    helperText: 'MSSQL'
+                },
+                {
+                    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Database-mysql.svg',
+                    imgTitle: 'RRZEicons [CC BY-SA (https://creativecommons.org/licenses/by-sa/3.0)]',
+                    imgAlt: 'MySql-logo',
+                    helperText: 'MySQL'
+                }
+            ]
+        }
+    ]
+}
