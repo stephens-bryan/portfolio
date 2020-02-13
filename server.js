6d1fbc478b65233a748ff5667bf4c56d278522b5
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-02-13 15:47:35 -0500

    modifed server.js

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
 const express = require('express');
-
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
 
-app.use(express.static('public'));
+app.use(express.static('build'));
 
-app.listen(3001, function(){
+app.listen(PORT, function(){
     console.log(`app running on port ${port}`);
 });
-
-// app.get('/', (req, res) => res.send('Hello World from Express.js!'));
-
-// app.listen(port, () => console.log(`App listening on port ${port}`));
-
-// http.createServer(function(req,res){
-
-//     res.statusCode = 200;
-
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello World');
-
-// }).listen(3000);
-
-// console.log(`File server running at port 3000`);
