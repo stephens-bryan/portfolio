04fb5b4a4fc2f9fa90977343087772f4041edbff
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-02-18 10:50:08 -0500

    added gzip compression

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const compression = require('compression');
 const PORT = process.env.PORT || 3001;
 
-
+app.use(compression());
 app.use(express.static('build'));
 
 app.listen(PORT, function(){
