1637b1c768edb628da4da7983a0c2e16a79bbe5a
Bryan Stephens <bryanstephens@Bryans-MacBook-Pro-2.local>
2020-02-18 15:11:38 -0500

    setup Introsection observer listeners

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 import Hexagon from '../Partials/Hexagon/index';
 
+import 'intersection-observer';
+
 import './main.scss';
 
 const Main = ({props}) => {
@@ -9,28 +11,45 @@ const Main = ({props}) => {
     let ref = React.createRef();
 
     const isElementInViewport = (el) => {
-        const rect = el.getBoundingClientRect();
+        let rect = el.getBoundingClientRect();
 
         return (
             rect.top >= 0 &&
             rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            rect.bottom <= ( window.innerHeight || document.documentElement.clientHeight ) &&
+            rect.right <= ( window.innerWidth || document.documentElement.clientWidth )
         );
     }
+
+    const observer = new IntersectionObserver(
+        ( [entry] ) => {
+
+            if( entry.intersectionRatio === 1 ){
+                entry.target.classList.add( "in-view" );
+            }
+        },
+        {
+            root: null,
+            rootMargin : '0px',
+            threshold: 1.0
+        }
+    );
 
     useEffect(() => {
 
         let checkInViewPort = () => {
             [...ref.current.childNodes].map( item => {
 
-            if( isElementInViewport( item ) ){
-                item.classList.add( "in-view" );
-            }
+            // // if( isElementInViewport( item ) ){
+            // //     item.classList.add( "in-view" );
+            // // }
+
+            observer.observe(item);
             });
         };
 
         window.addEventListener( 'DOMContentLoaded', checkInViewPort );
+        window.addEventListener( 'touchmove' , checkInViewPort );
         window.addEventListener( 'scroll', checkInViewPort );
 
     }, []);
