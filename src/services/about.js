89a72280f39e188ad108aaceb894d1849ba67701
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2020-02-18 10:50:48 -0500

    modified service content

--- src/services/about.js
+++ src/services/about.js
@@ -6,6 +6,6 @@ export default {
     },
     content: [
         { text: "I am a web developer with 2+ years in developing, testing, and debugging accessible, database-driven web sites and applications. I love exploring new technologies and developing my skills any chance I get."},
-        { text: "When not working on the web, I consider myself to be a bit of a gastronome and love to explore new restuarants and cusines when I get a chance."}
+        { text: "When not working on the web, I consider myself to be a bit of a gastronome and love to explore new restuarants and cuisines when I get a chance."}
     ]
 }
